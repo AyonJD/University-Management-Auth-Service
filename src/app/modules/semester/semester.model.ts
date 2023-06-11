c2929b30f9fd35c11d5a3e3aaba5f871b70bb500
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-06-11 15:38:11 +0600

    [Done] Pre hook for checking if same year and semester exists before creating new one

--- src/app/modules/semester/semester.model.ts
+++ src/app/modules/semester/semester.model.ts
@@ -5,6 +5,8 @@ import {
   SemesterCode,
   SemesterTitle,
 } from '../../../constant/semester.constant'
+import ApiError from '../../../errors/ApiError'
+import status from 'http-status'
 
 const semesterSchema = new Schema<ISemester>(
   {
@@ -38,6 +40,21 @@ const semesterSchema = new Schema<ISemester>(
 
 // Static methods:
 // semesterSchema.statics.findUserById = async function (id: string) { }
+
+// Pre hook for checking if same year and semester exists before creating new one
+semesterSchema.pre('save', async function (next) {
+  const semester = this as ISemester
+  const sameSemester = await semesterModel.findOne({
+    title: semester.title,
+    year: semester.year,
+  })
+
+  if (sameSemester) {
+    throw new ApiError(status.CONFLICT, 'Semester already exists')
+  }
+
+  next()
+})
 
 const semesterModel = model<ISemester, ISemesterModel>(
   'Semester',
