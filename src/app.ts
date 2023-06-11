6bbf7a085806681329dfaa0fb26e12919ad6a961
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-06-11 17:27:23 +0600

    [Done] Handle forbidden route

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler'
 
 // Import routes
 import routes from './app/Routes/index'
+import ApiError from './errors/ApiError'
+import httpStatus from 'http-status'
 
 const app: Application = express()
 
@@ -21,5 +23,20 @@ app.use('/api/v1', routes)
 
 // Global error handler
 app.use(globalErrorHandler)
+
+// Forbidden routes
+app.all('*', (req, res, next) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    status: 'false',
+    message: `No API endpoint found for ${req.method} ${req.originalUrl}`,
+    errorMessages: [
+      {
+        message: `No API endpoint found for ${req.method} ${req.originalUrl}`,
+        path: req.originalUrl,
+      },
+    ],
+    stack: '',
+  })
+})
 
 export default app
