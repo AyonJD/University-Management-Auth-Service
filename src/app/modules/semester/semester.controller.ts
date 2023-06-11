45983c978d17df70008e25c07b1815f512956528
Ayon Jodder <83225232+AyonJD@users.noreply.github.com>
2023-06-11 16:39:01 +0600

    [Done] Custom try catch function

--- src/app/modules/semester/semester.controller.ts
+++ src/app/modules/semester/semester.controller.ts
@@ -1,17 +1,14 @@
-import { RequestHandler } from 'express'
+import { Request, Response } from 'express'
 import { SemesterService } from './semester.service'
 import { sendSuccessResponse } from '../../utils/customResponse'
+import catchAsync from '../../../shared/catchAsync'
 
-const createSemester: RequestHandler = async (req, res, next) => {
-  try {
-    const semesterData = req.body
-    const result = await SemesterService.createSemester(semesterData)
+const createSemester = catchAsync(async (req: Request, res: Response) => {
+  const semesterData = req.body
+  const result = await SemesterService.createSemester(semesterData)
 
-    sendSuccessResponse(res, result, 'Semester created successfully')
-  } catch (error: any) {
-    next(error)
-  }
-}
+  sendSuccessResponse(res, result, 'Semester created successfully')
+})
 
 export const SemesterController = {
   createSemester,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,16 +1,12 @@
-import { RequestHandler } from 'express'
-import { z } from 'zod'
+import { Request, Response } from 'express'
 import { sendSuccessResponse } from '../../utils/customResponse'
 import { UserService } from './user.service'
+import catchAsync from '../../../shared/catchAsync'
 
-const createUser: RequestHandler = async (req, res, next) => {
-  try {
-    const result = await UserService.createUserService(req.body)
-    sendSuccessResponse(res, result, 'User created successfully')
-  } catch (error: any) {
-    next(error)
-  }
-}
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const result = await UserService.createUserService(req.body)
+  sendSuccessResponse(res, result, 'User created successfully')
+})
 
 export const UserController = {
   createUser,
